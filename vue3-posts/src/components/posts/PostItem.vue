3e38cf81b3bc358a250a51998a38521eaa16f67f
dk_sungmin <sungmin_hwang@daekyo.co.kr>
2023-02-08 13:13:47 +0900

    게시판 만들기

---
+++ vue3-posts/src/components/posts/PostItem.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title">{{ title }}</h5>
+			<p class="card-text">
+				{{ contents }}
+			</p>
+			<p class="text-muted">{{ createAt }}</p>
+		</div>
+	</div>
+</template>
+
+<script setup="setup">
+defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	contents: {
+		type: String,
+	},
+	createAt: {
+		type: [String, Date, Number],
+	},
+});
+</script>
+
+<style lang="scss" scoped="scoped"></style>
